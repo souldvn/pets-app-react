adc197c0bea3e6d472b11861eee39daf001043d5
souldvn <emilevil97@gmail.com>
2023-05-25 18:11:35 +0300

    init

--- src/components/Kitchen/Kitchen.jsx
+++ src/components/Kitchen/Kitchen.jsx
@@ -3,12 +3,25 @@ import styles from './Kitchen.module.css'
 import pet from '../../images/котик.png'
 import KitchenPanel from "./KitchenPanel";
 import Interface from "../Interface/Interface";
+import home from '../../images/домой.png'
+import {useNavigate} from "react-router-dom";
 
 const Kitchen = () => {
+
+    const navigate = useNavigate()
+
+    const handleClick = (path) =>{
+        navigate(path)
+    }
+
     return (
         <div className={styles.wrapper}>
             <Interface/>
             <img className={styles.mainpic} src={pet} alt=''/>
+            <div onClick={() => handleClick('/')} className={styles.home}>
+                <img  src={home} alt=''/>
+            </div>
+
             <KitchenPanel/>
         </div>
     );
