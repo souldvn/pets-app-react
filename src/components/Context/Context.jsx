62c333d7b9cbbef16050a8b32a56f0908f01edfe
souldvn <emilevil97@gmail.com>
2023-06-09 14:10:58 +0300

    init

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -15,32 +15,50 @@ const BarWidthProvider = ({ children }) => {
 
     useEffect(() => {
         localStorage.setItem('barWidth', barWidth);
+        localStorage.setItem('lastUpdateTime', Date.now());
     }, [barWidth]);
 
     useEffect(() => {
         localStorage.setItem('energyLevel', energyLevel);
+        localStorage.setItem('lastUpdateTime', Date.now());
     }, [energyLevel]);
 
     useEffect(() => {
         localStorage.setItem('emotional', emotional);
+        localStorage.setItem('lastUpdateTime', Date.now());
     }, [emotional]);
 
     useEffect(() => {
-        const handleBeforeUnload = () => {
-            localStorage.setItem('barWidth', barWidth);
-            localStorage.setItem('energyLevel', energyLevel);
-            localStorage.setItem('emotional', emotional);
+        const lastUpdateTime = localStorage.getItem('lastUpdateTime');
+        const timeDiff = Date.now() - lastUpdateTime;
+
+        const decreaseValues = () => {
+            setBarWidth((prevWidth) => {
+                const newWidth = parseFloat(prevWidth) - (timeDiff / 1000) * 0.05;
+                const clampedWidth = clampValue(newWidth, 1, 100);
+                return `${clampedWidth}%`;
+            });
+
+            setEnergyLevel((prevEnergyLevel) => {
+                const newEnergyLevel = prevEnergyLevel - (timeDiff / 1000) * 0.05;
+                return clampValue(newEnergyLevel, 1, 100);
+            });
+
+            setEmotional((prevEmotional) => {
+                const newEmotional = prevEmotional - (timeDiff / 1000) * 0.05;
+                return clampValue(newEmotional, 1, 100);
+            });
         };
 
-        window.addEventListener('beforeunload', handleBeforeUnload);
+        decreaseValues();
 
-        return () => {
-            window.removeEventListener('beforeunload', handleBeforeUnload);
-        };
-    }, [barWidth, energyLevel, emotional]);
+        const intervalId = setInterval(decreaseValues, 1000);
 
-    const updateBarWidth = (newWidth) => {
-        setBarWidth(newWidth);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const clampValue = (value, min, max) => {
+        return Math.min(Math.max(value, min), max);
     };
 
     // Остальной код
@@ -49,7 +67,7 @@ const BarWidthProvider = ({ children }) => {
         <BarWidthContext.Provider
             value={{
                 barWidth,
-                updateBarWidth,
+                setBarWidth,
                 energyLevel,
                 setEnergyLevel,
                 emotional,
